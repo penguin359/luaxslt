f3640689998e5e2182145b460814215b732d331a
Loren M. Lang <lorenl@north-winds.org>
2017-06-12 22:37:08 -0700

    Attempt to stress test library with bad values

--- xml-test.lua
+++ xml-test.lua
@@ -3,4 +3,8 @@
 print("Hello, world!")
 --local doc = xml.parse_xml("test.xml")
 local doc = xml.parse_xml("<root><name>Hello</name></root>")
-print(xml.dump_xml(doc))
+local status, error = pcall(function() print('Parse XML Document:', xml.dump_xml(doc)     ) end) if not status then print('Lua error: '..error) end
+local status, error = pcall(function() print('Parse io.stdin:',     xml.dump_xml(io.stdin)) end) if not status then print('Lua error: '..error) end
+local status, error = pcall(function() print('io.stdin:',           io.stdin              ) end) if not status then print('Lua error: '..error) end
+local status, error = pcall(function() print('Parse empty string:', xml.dump_xml('')      ) end) if not status then print('Lua error: '..error) end
+local status, error = pcall(function() print('Parse nil:',          xml.dump_xml(nil)     ) end) if not status then print('Lua error: '..error) end
